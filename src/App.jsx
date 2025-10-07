f208854a953041624c1e69245675b59739ce18e4
Game Fire <sudhanshugaming37@gmail.com>
2025-10-07 16:58:59 +0530

    code :components,screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Input from './Components/Input';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='h-screen w-screen flex justify-center items-center' >
-      <h1 className='bg-green-500 text-4xl text-white '> BargainBox </h1>
-    </div>
-  )
+ <div>
+        <Input/>
+  </div> 
+
+  );
 }
 
-export default App
+
+export default App;
